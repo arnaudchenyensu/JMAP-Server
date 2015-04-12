cddd7e62a7524399c84571368564b88fea3d5206
Arnaud <arnaud.chenyensu@gmail.com>
2015-04-12 10:20:24 +0400

    Add model for mailbox

--- models.js
+++ models.js
@@ -10,4 +10,21 @@ models.Account = function (opts) {
     this.capabilities = opts.capabilities || null;
 };
 
+models.Mailbox = function (opts) {
+    this.name = opts.name || null ;
+    this.parentId = opts.parentId || null;
+    this.role = opts.role || null;
+    this.precedence = opts.precedence || null;
+    this.mustBeOnlyMailbox = opts.mustBeOnlyMailbox || null;
+    this.mayReadMessageList = opts.mayReadMessageList || null;
+    this.mayAddMessages = opts.mayAddMessages || null;
+    this.mayRemoveMessages = opts.mayRemoveMessages || null;
+    this.mayCreateChild = opts.mayCreateChild || null;
+    this.mayRenameMailbox = opts.mayRenameMailbox || null;
+    this.mayDeleteMailbox = opts.mayDeleteMailbox || null;
+    this.totalMessages = opts.totalMessages || null;
+    this.unreadMessages = opts.unreadMessages || null;
+    this.totalThreads = opts.totalThreads || null;
+    this.unreadThreads = opts.unreadThreads || null;
+};
 module.exports = models;
---
+++ spec/models/mailboxSpec.js
@@ -0,0 +1,68 @@
+var models = require('../../models.js');
+
+describe("A mailbox", function() {
+
+    var opts = {};
+    var mailbox = new models.Mailbox(opts);
+
+    it("has the property name", function() {
+        expect(mailbox.name).toBeDefined();
+    });
+
+    it("has the property parentId", function() {
+        expect(mailbox.parentId).toBeDefined();
+    });
+
+    it("has the property role", function() {
+        expect(mailbox.role).toBeDefined();
+    });
+
+    it("has the property precedence", function() {
+        expect(mailbox.precedence).toBeDefined();
+    });
+
+    it("has the property mustBeOnlyMailbox", function() {
+        expect(mailbox.mustBeOnlyMailbox).toBeDefined();
+    });
+
+    it("has the property mayReadMessageList", function() {
+        expect(mailbox.mayReadMessageList).toBeDefined();
+    });
+
+    it("has the property mayAddMessages", function() {
+        expect(mailbox.mayAddMessages).toBeDefined();
+    });
+
+    it("has the property mayRemoveMessages", function() {
+        expect(mailbox.mayRemoveMessages).toBeDefined();
+    });
+
+    it("has the property mayCreateChild", function() {
+        expect(mailbox.mayCreateChild).toBeDefined();
+    });
+
+    it("has the property mayRenameMailbox", function() {
+        expect(mailbox.mayRenameMailbox).toBeDefined();
+    });
+
+    it("has the property mayDeleteMailbox", function() {
+        expect(mailbox.mayDeleteMailbox).toBeDefined();
+    });
+
+    it("has the property totalMessages", function() {
+        expect(mailbox.totalMessages).toBeDefined();
+    });
+
+    it("has the property unreadMessages", function() {
+        expect(mailbox.unreadMessages).toBeDefined();
+    });
+
+    it("has the property totalThreads", function() {
+        expect(mailbox.totalThreads).toBeDefined();
+    });
+
+    it("has the property unreadThreads", function() {
+        expect(mailbox.unreadThreads).toBeDefined();
+    });
+
+});
