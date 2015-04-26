5690c806deec4012d8837dcacbf8efce087710d5
Arnaud <arnaud.chenyensu@gmail.com>
2015-04-26 15:38:37 +0400

    Refactor db according to new models

--- db.js
+++ db.js
@@ -20,8 +20,7 @@ methods.setMailboxes = function (results, args, callId) {
 
     // TODO defaults to primary account, how we do that???
     // maybe check tokens, cookies...?
-    res.accountId = args.accountId;
-    res.accountId = "test";
+    res.accountId = args.accountId || "test";
 
     if (args.ifInState) {
         // TODO check state
@@ -33,6 +32,15 @@ methods.setMailboxes = function (results, args, callId) {
         res.created = {};
         _.keys(args.create).forEach(function (creationId) {
             promises.push(methods.setMailboxes.createMailbox(res, creationId, args.create[creationId]));
+        });
+    }
+
+    // update Mailboxes
+    if (args.update) {
+        res.notUpdated = {};
+        res.updated = [];
+        _.keys(args.update).forEach(function (mailboxId) {
+            promises.push(methods.setMailboxes.updateMailbox(res, mailboxId, args.update[mailboxId]));
         });
     }
 
@@ -48,68 +56,26 @@ methods.setMailboxes.createMailbox = function (res, creationId, mailbox) {
         properties: []
     };
 
-    var properties = _.keys(mailbox);
+    var newMailbox = new models.Mailbox(mailbox, false);
 
-    var mayXXX = [
-        'mayReadMessageList', 'mayAddMessages', 'mayRemoveMessages',
-        'mayCreateChild', 'mayRenameMailbox', 'mayDeleteMailbox'
-    ];
-
-    var otherProperties = [
-        'totalMessages', 'unreadMessages', 'totalThreads', 'unreadThreads'
-    ];
-
-    var validRoles = [
-        'inbox', 'archive', 'drafts', 'outbox', 'sent',
-        'trash', 'spam', 'templates'
-    ];
-
-    function isRoleValid(role) {
-        if (role === null || validRoles.indexOf(role) !== -1 || _.startsWith("x-")) {
-            return true;
-        }
-        return false;
-        // TODO No two mailboxes may have the same role
-    }
-
-    // Validate each property
-    properties.forEach(function (property) {
-        if (property === "id" && mailbox[property]) {
-            setError.properties.push("id");
-        } else if (property === "parentId" && mailbox[property] !== null) {
-            // TODO need to check other things, see spec
-            setError.properties.push("parentId");
-        } else if (property === "role" && (!isRoleValid(mailbox[property]) || mailbox[property] !== null)) {
-            setError.properties.push("role");
-        } else if (property === "mustBeOnlyMailbox") {
-            setError.properties.push("mustBeOnlyMailbox");
-        } else if ((mayXXX.indexOf(property) !== -1) && mailbox[property] !== true) {
-            setError.properties.push(property);
-        } else if ((otherProperties.indexOf(property) !== -1) && mailbox[property] !== 0) {
-            setError.properties.push(property);
-        }
-    });
-
-    if (setError.properties.length !== 0) {
+    if (newMailbox.__invalidProperties) {
+        setError.properties = newMailbox.__invalidProperties;
         res.notCreated[creationId] = setError;
         return Promise.resolve(setError);
     } else {
-        var newMailbox = new models.Mailbox(mailbox);
         var _id = res.accountId + "_mailbox_" + uuid.v1(),
             mustBeOnlyMailbox = true;
         newMailbox._id = _id;
         newMailbox.mustBeOnlyMailbox = true;
-        return db.put(newMailbox)
-            .then(function () {
-                res.created[creationId] = {
-                    id: _id,
-                    mustBeOnlyMailbox: mustBeOnlyMailbox
-                };
-                return newMailbox;
-            })
-            .catch(function (err) {
-                console.log(err);
-            });
+        return db.put(newMailbox).then(function () {
+            res.created[creationId] = {
+                id: _id,
+                mustBeOnlyMailbox: mustBeOnlyMailbox
+            };
+            return newMailbox;
+        }).catch(function (err) {
+            console.log(err);
+        });
     }
 };
 module.exports = methods;
