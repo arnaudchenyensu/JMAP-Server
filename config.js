0e9e7545be88525672827dbc154392f7d1266cec
Arnaud <arnaud.chenyensu@gmail.com>
2015-05-08 13:51:23 +0400

    Bidirectional replicate

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var PouchDB = require('pouchdb');
-PouchDB.replicate('jmap', 'http://localhost:5984/jmap', {live: true});
+PouchDB.sync('jmap', 'http://localhost:5984/jmap', {live: true});
 
 module.exports = {
     'db': new PouchDB('jmap')
