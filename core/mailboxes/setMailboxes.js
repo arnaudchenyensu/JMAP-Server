0e5ce8cc422ed4db1615b9fe459f439911fe471b
Arnaud <arnaud.chenyensu@gmail.com>
2015-05-14 14:23:37 +0400

    Refactor function set, create, update

--- core/mailboxes/setMailboxes.js
+++ core/mailboxes/setMailboxes.js
@@ -1,107 +1,24 @@
-var config  = require('../../config.js');
-var db      = config.db;
-var models  = require('../../models.js');
-var Promise = require('bluebird');
-var _       = require('lodash');
-var uuid    = require('node-uuid');
+var models = require('../../models.js');
+var utils  = require('../utils.js');
 
 var setMailboxes = function (results, args, callId) {
     var responseName = "mailboxesSet";
-    var res = {};
+    var response = {};
     var promises = [];
 
     // TODO defaults to primary account, how we do that???
     // maybe check tokens, cookies...?
-    res.accountId = args.accountId || "test";
+    response.accountId = args.accountId || "test@test.com";
 
-    if (args.ifInState) {
-        // TODO check state
-    }
-
-    // create Mailboxes
-    if (args.create) {
-        res.notCreated = {};
-        res.created = {};
-        _.keys(args.create).forEach(function (creationId) {
-            promises.push(createMailbox(res, creationId, args.create[creationId]));
-        });
-    }
-
-    // update Mailboxes
-    if (args.update) {
-        res.notUpdated = {};
-        res.updated = [];
-        _.keys(args.update).forEach(function (mailboxId) {
-            promises.push(updateMailbox(res, mailboxId, args.update[mailboxId]));
-        });
-    }
-
-    // destroy Mailboxes
-    if (args.destroy) {
-        res.notDestroyed = {};
-        res.destroyed = {};
-        args.destroy.forEach(function (mailboxId) {
-            promises.push(destroyMailbox(res, mailboxId));
-        });
-    }
-
-    return Promise.all(promises).then(function () {
-        results.push([responseName, res, callId]);
+    return utils.set(response, args, models.mailbox).then(function () {
+        results.push([responseName, response, callId]);
+    }).catch(function (err) {
+        console.log(err);
     });
-};
-
-var createMailbox = function (res, creationId, mailbox) {
-    var setError = {
-        type: "invalidProperties",
-        description: undefined,
-        properties: []
-    };
-
-    var newMailbox = models.mailbox.create(mailbox);
-
-    if (newMailbox.__invalidProperties) {
-        setError.properties = newMailbox.__invalidProperties;
-        res.notCreated[creationId] = setError;
-        return Promise.resolve(setError);
-    } else {
-        var _id = res.accountId + "_mailbox_" + uuid.v1(),
-            mustBeOnlyMailbox = true;
-        newMailbox._id = _id;
-        newMailbox.mustBeOnlyMailbox = true;
-        return db.put(newMailbox).then(function () {
-            res.created[creationId] = {
-                id: _id,
-                mustBeOnlyMailbox: mustBeOnlyMailbox
-            };
-            return newMailbox;
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
 };
 
 var updateMailbox = function (res, mailboxId, updatedProperties) {
-    var setError = {
-        type: "invalidProperties",
-        description: undefined,
-        properties: []
-    };
 
-    // TODO notFound error for the id
-    return db.get(mailboxId).then(function (mailbox) {
-        var updatedMailbox = models.mailbox.update(mailbox, updatedProperties);
-        if (updatedMailbox.__invalidProperties) {
-            setError.properties = updatedMailbox.__invalidProperties;
-            res.notUpdated[mailboxId] = setError;
-            return Promise.resolve(setError);
-        } else {
-            return db.put(mailbox).then(function () {
-                res.updated.push(mailboxId);
-            }).catch(function (err) {
-                console.log(err);
-            });
-        }
-    });
 };
 
 var destroyMailbox = function (res, mailboxId) {
--- core/utils.js
+++ core/utils.js
@@ -1,6 +1,7 @@
-var config = require('../config.js');
-var db     = config.db;
-var _      = require('lodash');
+var config  = require('../config.js');
+var db      = config.db;
+var _       = require('lodash');
+var Promise = require('bluebird');
 
 var utils = {};
 
@@ -45,6 +46,101 @@ utils.get = function (response, args, startkey, endkey) {
     }).catch(function (err) {
         console.log(err);
     });
+};
+
+utils.set = function (response, args, model) {
+    var promises = [];
+
+    if (args.ifInState) {
+        // TODO check state
+    }
+
+    // create Mailboxes
+    if (args.create) {
+        response.notCreated = {};
+        response.created = {};
+        _.keys(args.create).forEach(function (creationId) {
+            promises.push(create(response, creationId, args.create[creationId], model));
+        });
+    }
+
+    // update Mailboxes
+    if (args.update) {
+        response.notUpdated = {};
+        response.updated = [];
+        _.keys(args.update).forEach(function (mailboxId) {
+            promises.push(update(response, mailboxId, args.update[mailboxId], model));
+        });
+    }
+
+    // destroy Mailboxes
+    if (args.destroy) {
+        response.notDestroyed = {};
+        response.destroyed = {};
+        args.destroy.forEach(function (mailboxId) {
+            promises.push(destroy(response, mailboxId, model));
+        });
+    }
+
+    return Promise.all(promises);
+};
+
+var create = function (response, creationId, obj, model) {
+    var setError = {
+        type: "invalidProperties",
+        description: undefined,
+        properties: []
+    };
+
+    var newObj = model.create(obj);
+
+    if (newObj.__invalidProperties) {
+        setError.properties = newObj.__invalidProperties;
+        response.notCreated[creationId] = setError;
+        return Promise.resolve(setError);
+    } else {
+        var _id = model.id(response.accountId),
+            mustBeOnlyMailbox = true;
+        newObj._id = _id;
+        newObj.mustBeOnlyMailbox = true;
+        return db.put(newObj).then(function () {
+            response.created[creationId] = {
+                id: _id,
+                mustBeOnlyMailbox: mustBeOnlyMailbox
+            };
+            return newObj;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+};
+
+var update = function (response, objId, updatedProperties, model) {
+    var setError = {
+        type: "invalidProperties",
+        description: undefined,
+        properties: []
+    };
+
+    // TODO notFound error for the id
+    return db.get(objId).then(function (obj) {
+        var updatedObj = model.update(obj, updatedProperties);
+        if (updatedObj.__invalidProperties) {
+            setError.properties = updatedObj.__invalidProperties;
+            response.notUpdated[objId] = setError;
+            return Promise.resolve(setError);
+        } else {
+            return db.put(obj).then(function () {
+                response.updated.push(objId);
+            }).catch(function (err) {
+                console.log(err);
+            });
+        }
+    });
+};
+
+var destroy = function (response, objId) {
+    // TODO
 };
 
 module.exports = utils;
