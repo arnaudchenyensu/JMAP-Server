e3b6136230b6722e18d5752ca79ad2221ac1823c
Arnaud <arnaud.chenyensu@gmail.com>
2015-08-14 15:54:38 +0400

    Remove useless code

--- models/thread.js
+++ models/thread.js
@@ -3,9 +3,6 @@ var uuid    = require('node-uuid');
 var utils   = require('../utils.js');
 var thread  = {};
 
-// immutables or can only be updated by the server
-var immutables = ["id"];
-
 thread.id = function (accountId) {
     var date = (new Date().toJSON()).split('.')[0] + 'Z';
     return accountId + '_thread_' + date + uuid.v1();
@@ -90,60 +87,7 @@ thread.methods = {
             }
         },
         responseName: "threads"
-    },
-    getThreadUpdate: {
-        func: utils.getUpdates,
-        request: {
-
-        },
-        response: {
-
-        },
-        responseName: "threadUpdates"
     }
-};
-thread.create = function (opts) {
-    // TODO sort messageIds
-    return {
-        messageIds: opts.messageIds || []
-    };
-};
-
-thread.update = function (thread, updatedProperties) {
-    var properties = _.keys(updatedProperties);
-    var invalidProperties = [];
-
-    if (updatedProperties.name) {
-        // TODO valid UTF-8
-    }
-
-    if (updatedProperties.hasOwnProperty("parentId") && updatedProperties.parentId !== null) {
-        // TODO need to check something else
-        invalidProperties.push("parentId");
-    }
-
-    properties.forEach(function (property) {
-        if (_.contains(immutables, property)) {
-            invalidProperties.push(property);
-        }
-    });
-
-    if (invalidProperties.length !== 0) {
-        // do not create the object
-        return {__invalidProperties: invalidProperties};
-    } else {
-        // If there are no errors, return the new updated thread
-        _.keys(thread).forEach(function (key) {
-            if (_.contains(properties, key)) {
-                thread[key] = updatedProperties[key];
-            }
-        });
-        return thread;
-    }
-};
-
-thread.destroy = function (mailboxId) {
-
 };
 
 module.exports = thread;
